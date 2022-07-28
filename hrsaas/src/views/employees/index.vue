3ae0077a2870e86d8043d561aeb261e2c06cb77f
zhangmengyang <2655419131@qq.com>
2022-07-28 15:06:58 +0800

    员工管理 头部插槽

--- hrsaas/src/views/employees/index.vue
+++ hrsaas/src/views/employees/index.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <TooBar>
+        <template #before>
+          <span>共500条数据</span>
+        </template>
+
+        <template #after>
+          <el-button type="primary" size="mini">导入</el-button>
+          <el-button type="danger" size="mini">导出</el-button>
+          <el-button type="warning" size="mini">新增员工  </el-button>
+        </template>
+      </TooBar>
     </div>
   </div>
 </template>
 
 <script>
+import TooBar from "../../components/TooBar";
 export default {
-
-}
+  components: {
+    TooBar,
+  },
+};
 </script>
 
-<style>
-
-</style>
-
+<style></style>
--- hrsaas/src/views/setting/index.vue
+++ hrsaas/src/views/setting/index.vue
@@ -3,7 +3,11 @@
     <div class="app-container">
       <el-tabs v-model="activeName" @tab-click="handleClick">
         <el-tab-pane label="用户管理" name="first">
-          <el-button icon="el-icon-plus" type="primary" size="small"
+          <el-button
+            icon="el-icon-plus"
+            type="primary"
+            size="small"
+            @click="dialogVisible = true"
             >新增角色</el-button
           >
 
@@ -14,9 +18,15 @@
             </el-table-column>
             <el-table-column prop="description" label="描述"> </el-table-column>
             <el-table-column label="操作">
-              <el-button type="success" size="small">分配权限</el-button>
-              <el-button type="primary" size="small">编辑</el-button>
-              <el-button type="danger" size="small">删除</el-button>
+              <template v-slot="{ row }">
+                <el-button type="success" size="small">分配权限</el-button>
+                <el-button type="primary" size="small" @click="edit(row.id)"
+                  >编辑</el-button
+                >
+                <el-button type="danger" size="small" @click="delBtn(row.id)"
+                  >删除</el-button
+                >
+              </template>
             </el-table-column>
           </el-table>
 
@@ -67,11 +77,37 @@
         </el-tab-pane>
       </el-tabs>
     </div>
+    <el-dialog :title="title" :visible="dialogVisible" @close="close">
+      <el-form
+        ref="roleForm"
+        :model="roleFormData"
+        :rules="roleRules"
+        label-width="120px"
+      >
+        <el-form-item label="角色" prop="name">
+          <el-input v-model="roleFormData.name"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" prop="description">
+          <el-input v-model="roleFormData.description"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="close">取 消</el-button>
+        <el-button type="primary" @click="submitOk">确 定</el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getRoleList, getCompanyInfo } from "../../api/setting";
+import {
+  getRoleList,
+  getCompanyInfo,
+  deleteRole,
+  getRoleDetail,
+  updateRole,
+  addRole,
+} from "../../api/setting";
 export default {
   data() {
     return {
@@ -79,15 +115,33 @@ export default {
       tableData: [],
       page: {
         page: 1,
-        pagesize: 2,
+        pagesize: 10,
         total: 0,
       },
       fromData: {},
+      dialogVisible: false,
+      roleFormData: {
+        name: "",
+        description: "",
+      },
+      roleRules: {
+        name: [
+          {
+            required: true,
+            message: "角色必填项",
+          },
+        ],
+      },
     };
   },
   created() {
     this.getRoleList();
     this.getCompanyInfo();
+  },
+  computed: {
+    title() {
+      return this.roleFormData?.id ? "编辑" : "新增";
+    },
   },
   methods: {
     // 角色列表数据
@@ -108,6 +162,59 @@ export default {
     async getCompanyInfo() {
       this.fromData = await getCompanyInfo(this.$store.getters.companyId);
       // console.log(this.fromData);
+    },
+    // 删除
+    async delBtn(id) {
+      try {
+        // 通过插槽获取的id 来确定那一条数据
+        await this.$confirm("确定删除吗？");
+        // 获取当前界面删除之前的数据长度
+        const len = this.tableData.length;
+        // 如果删除之前的数据长度为1，同时当前页码数不是第一页的情况下 页码自减一
+        if (len === 1 && this.page.page > 1) {
+          this.page--;
+        }
+        // 删除
+        await deleteRole(id);
+        // 成功提示
+        this.$message.success("删除成功");
+        // 重新渲染页面
+        this.getRoleList();
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    // 编辑
+    async edit(id) {
+      // 弹层显示
+      this.dialogVisible = true;
+      // 调用接口 通过插槽row传过来的id 实现数据回显
+      this.roleFormData = await getRoleDetail(id);
+    },
+    // 取消
+    close() {
+      this.dialogVisible = false;
+      // 清空表单效验
+      this.$refs.roleForm.resetFields();
+      // 把表单数据置空
+      this.roleFormData = {
+        name: "",
+        description: "",
+      };
+    },
+    // 确定
+    async submitOk() {
+      // 先进行表单效验
+      this.$refs.roleForm.validate();
+      // 判断是否是编辑 或者新增 进行提交表单
+      if (this.roleFormData.id) {
+        await updateRole(this.roleFormData);
+      } else {
+        await addRole(this.roleFormData);
+      }
+      this.$message.success("成功");
+      this.getRoleList();
+      this.close();
     },
 
     handleClick() {},
